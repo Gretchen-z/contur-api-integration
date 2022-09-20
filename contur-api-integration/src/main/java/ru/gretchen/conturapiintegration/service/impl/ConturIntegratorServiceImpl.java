52299e8b39e99dcf5019a18725461ff56f368945
Gretchen <rysrit@gmail.com>
2022-09-20 13:49:43 +0300

    refactoring

--- contur-api-integration/src/main/java/ru/gretchen/conturapiintegration/service/impl/ConturIntegratorServiceImpl.java
+++ contur-api-integration/src/main/java/ru/gretchen/conturapiintegration/service/impl/ConturIntegratorServiceImpl.java
@@ -2,8 +2,6 @@ package ru.gretchen.conturapiintegration.service.impl;
 
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.gretchen.conturapiintegration.exception.GetBriefReportException;
 import ru.gretchen.conturapiintegration.exception.SendingRequestToConturApiException;
@@ -16,6 +14,9 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +33,7 @@ public class ConturIntegratorServiceImpl implements ConturIntegratorService {
         try {
             request = HttpRequest.newBuilder()
                     .uri(new URI(URI_BRIEF_REPORT))
+                    .timeout(Duration.of(10, SECONDS))
                     .header("Content-Type", "application/json")
                     .headers("inn", inn, "key", KEY)
                     .GET()
